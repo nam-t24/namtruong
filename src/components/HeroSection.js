5ce1dd4aaab4989c4e0292b5a58fea84be065e6d
nam-t24 <namtruong831@gmail.com>
2020-12-08 21:43:28 -0600

    Home page

---
+++ src/components/HeroSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+import './HeroSection.css';
+
+function HeroSection() {
+    return (
+        <div className = 'hero-container'>
+            
+            <h1>Nam Truong</h1>
+            <p>Class of 2020 - B.S. in Computer Science</p>
+        </div>
+    )
+}
+
+export default HeroSection;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,7 +31,7 @@ function Navbar() {
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
             NT
-            <i class='fab fa-typo3' />
+            {/*<i class='fab fa-typo3' />*/}
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
