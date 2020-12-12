9fb0c2839cca867d826b1e32baeb1c472ab9c7d7
nam-t24 <namtruong831@gmail.com>
2020-12-11 20:18:55 -0600

    updated resume

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -2,5 +2,5 @@ import React from 'react';
 import '../../App.css';
 
 export default function Project(){
-    return <h1 className='projects'>Hola!</h1>
+    return <p className='projects'>Coming soon!</p>
 }
